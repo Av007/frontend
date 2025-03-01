71576397595966b5961192a78334c036797b522a
Av007 <avdeevvladimir@gmail.com>
2025-03-01 23:23:01 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import ButtonUsage from './components/ButtonComponent'
+
+function App() {
+
+  return (
+    <>
+      <ButtonUsage/>
+    </>
+  )
+}
+
+export default App
